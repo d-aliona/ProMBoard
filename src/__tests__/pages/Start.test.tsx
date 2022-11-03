bb898992526855932519fb106396000bb8e124dc
d-aliona <danxaliona@gmail.com>
2022-11-03 20:30:08 +0200

    write tests to check navigation on start page

--- src/__tests__/pages/Start.test.tsx
+++ src/__tests__/pages/Start.test.tsx
@@ -1,20 +1,62 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render, screen, cleanup, fireEvent, waitFor} from '@testing-library/react';
 import "@testing-library/jest-dom";
-import { createMemoryHistory } from 'history';
+// import { createMemoryHistory } from 'history';
+// import {useNavigate} from 'react-router-dom';
+   
+import  userEvent from '@testing-library/user-event';
+import { RenderInRouterWithStore } from '../../_test_helpers/RenderInRouterWithStore';
 import Start from '../../pages/Start';
-import Initials from '../../ui/Initials';
 
-test('check clicks', () => {
-    const testUser = {
-        email: null,
-        id: null,
-        firstName: 'firstName',
-        lastName: 'LastName',
-        guestBoards: ['1'],
-    }
+afterEach(cleanup);
 
-    render(<Start />)
+// const realLocation = window.location;
 
-    expect(screen.getByText('fL')).toBeInTheDocument;
+// beforeEach(() => {
+//   window.location = realLocation;
+// });
+
+// afterEach(() => {
+//   delete (window as any).location;
+// });
+
+
+test('check navigate to login page', async() => {
+    // const navigate = useNavigate();
+    // navigate('/')
+    // const testUser = {
+    //     email: null,
+    //     id: null,
+    //     firstName: 'firstName',
+    //     lastName: 'LastName',
+    //     guestBoards: ['1'],
+    // }
+    
+    const user = userEvent.setup();
+    
+    render(<Start />, { wrapper: RenderInRouterWithStore() });
+    
+    await waitFor(() => expect(window.location.pathname).toBe('/'))
+    
+    const spanElement = screen.getByText('Log in');
+    const parentDiv = spanElement.parentElement as HTMLElement;
+    
+    await user.click(parentDiv)
+    await waitFor(() => expect(window.location.pathname).toBe('/login'))
+})
+
+test('check navigate to signup page', async() => {
+    // window.location = realLocation;
+    // navigate('/');
+
+    const user = userEvent.setup();
+    // delete (window as any).location;
+    render(<Start />, { wrapper: RenderInRouterWithStore() });
+    
+    // await waitFor(() => expect(window.location.pathname).toBe('/login'))
+    
+    const buttonElement = screen.getByText('Sign up');
+        
+    await user.click(buttonElement)
+    await waitFor(() => expect(window.location.pathname).toBe('/signup'))
 })
---
+++ src/_test_helpers/RenderInRouterWithStore.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from '../store';
+
+export const RenderInRouterWithStore = () => ({ children }: any) => (
+  <BrowserRouter >
+      <Provider store={store}>
+          {children}
+      </Provider>
+  </BrowserRouter>
+);
